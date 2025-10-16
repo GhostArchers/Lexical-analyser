cef393f38d029597a8af7e282d239616da77277e
Goutham R <120236697+GhostArchers@users.noreply.github.com>
2025-10-16 16:41:56 +0530

    Update Utility.cpp

--- Utility.cpp
+++ Utility.cpp
@@ -1,20 +1,19 @@
-// Utility.cpp
 #include "Utility.h"
 
 std::string tokenTypeToString(TokenType type) {
     switch (type) {
-        case TokenType::KEYWORD: return "KEYWORD";
         case TokenType::IDENTIFIER: return "IDENTIFIER";
+        case TokenType::KEYWORD: return "KEYWORD";
         case TokenType::INTEGER: return "INTEGER";
         case TokenType::FLOAT: return "FLOAT";
         case TokenType::STRING: return "STRING";
         case TokenType::CHAR: return "CHAR";
         case TokenType::OPERATOR: return "OPERATOR";
         case TokenType::SEPARATOR: return "SEPARATOR";
-        case TokenType::EOF_TOKEN: return "EOF_TOKEN";
+        case TokenType::COMMENT: return "COMMENT";
         case TokenType::UNKNOWN: return "UNKNOWN";
-        default: return "UNKNOWN";
+        case TokenType::EOF_TOKEN: return "EOF";
     }
+    return "UNKNOWN";
 }
-
 
