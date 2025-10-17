262dd0ff185051b002905037d7a73a626ca1a6a4
Goutham R <120236697+GhostArchers@users.noreply.github.com>
2025-10-17 12:10:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,64 @@
-// main.cpp
-#include <iostream>
-#include <string>
-#include <vector>
-
 #include "Lexer.h"
-#include "Utility.h"
+#include <iostream>
+#include <iomanip>
 
-int main() {
-    std::cout << "=== Lexical Analyzer (C++) ===" << std::endl;
-
-    std::string input;
-    while (true) {
-        std::cout << "> ";
-        if (!std::getline(std::cin, input)) {
-            break;
-        }
-        if (input == "exit") {
-            break;
-        }
-
-        Lexer lexer(input);
-        std::cout << "--- Tokens ---" << std::endl;
-        while (true) {
-            Token token = lexer.nextToken();
-            if (token.tkType == TokenType::EOF_TOKEN) {
-                break;
-            }
-            std::cout
-                << "Type: " << tokenTypeToString(token.tkType)
-                << ", Value: '" << token.value << "'"
-                << ", Line: " << token.line
-                << ", Column: " << token.column
-                << std::endl;
-        }
+std::string tokenTypeToString(TokenType type) {
+    switch (type) {
+        case TokenType::IDENTIFIER: return "IDENTIFIER";
+        case TokenType::KEYWORD:    return "KEYWORD";
+        case TokenType::INTEGER:    return "INTEGER";
+        case TokenType::FLOAT:      return "FLOAT";
+        case TokenType::STRING:     return "STRING";
+        case TokenType::CHAR:       return "CHAR";
+        case TokenType::OPERATOR:   return "OPERATOR";
+        case TokenType::SEPARATOR:  return "SEPARATOR";
+        case TokenType::COMMENT:    return "COMMENT";
+        case TokenType::UNKNOWN:    return "UNKNOWN";
+        case TokenType::EOF_TOKEN:  return "EOF";
+        default:                    return "UNKNOWN";
     }
-
-    std::cout << "[Program Terminated]" << std::endl;
-    return 0;
 }
 
+int main() {
+    std::string input = R"(
+        int main() {
+            float num = 3.14;
+            char c = 'a';
+            string s = "Hello World";
+            // This is a comment
+            if (num > 1.0) {
+                num += 2;
+            }
+        }
+    )";
 
+    std::cout << "=== LEXICAL ANALYSIS START ===\n";
+    Lexer lexer(input);
+    
+    Token token;
+    int count = 1;
+
+    // Print table header
+    std::cout << std::left << std::setw(6) << "No."
+              << std::setw(15) << "Type"
+              << std::setw(20) << "Value"
+              << std::setw(8) << "Line"
+              << "Column\n";
+    std::cout << "--------------------------------------------------------------\n";
+
+    // Generate tokens
+    while (true) {
+        token = lexer.nextToken();
+        if (token.tkType == TokenType::EOF_TOKEN)
+            break;
+
+        std::cout << std::left << std::setw(6) << count++
+                  << std::setw(15) << tokenTypeToString(token.tkType)
+                  << std::setw(20) << token.value
+                  << std::setw(8) << token.line
+                  << token.column << "\n";
+    }
+
+    std::cout << "=== LEXICAL ANALYSIS COMPLETE ===\n";
+    return 0;
+}
